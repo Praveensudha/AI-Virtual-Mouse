519b1267b1ad188cd6cf4da94461ec7b953b6d4f
Rian8817 <rrian9037@gmail.com>
2025-11-02 10:00:00 +0530

    Enhance FPS display with descriptive text

--- aivirtualmouseproject.py
+++ aivirtualmouseproject.py
@@ -115,7 +115,7 @@ while True:
     cTime = time.time()
     fps = 1/(cTime-pTime)
     pTime = cTime
-    cv2.putText(img, str(int(fps)), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,0),3)
+    cv2.putText(img, f"FPS: {int(fps)}", (20, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,0),3)
    
     #12. Display
     
